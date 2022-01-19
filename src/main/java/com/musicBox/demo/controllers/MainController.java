0d4a27a2049b65bac72595eec652da067cd56535
oger_si <stas.oger.99@mail.ru>
2022-01-19 17:47:58 +0300

    create

---
+++ src/main/java/com/musicBox/demo/controllers/MainController.java
@@ -0,0 +1,28 @@
+package com.musicBox.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("title", "have we go again");
+        return "homePage";
+    }
+
+
+
+  //  public void example() {
+  //      String[] myString = new String[]{"раз", "два", "три", "четыре", "пять"};
+  //      int n = (int)Math.floor(Math.random() * myString.length);
+  //      System.out.println(myString[n]);
+ //   }
+
+
+}
---
+++ src/test/java/stas/MainControllerUnitTest.java
@@ -0,0 +1,17 @@
+//package stas;
+
+//import com.example.demo.controllers.MainController;
+//import org.junit.Test;
+
+//public class MainControllerUnitTest {
+
+
+//    private MainController controller = new MainController();
+
+ //   @Test
+ //   public void shouldReturnRandom_whenExample() {
+
+  //      controller.example();
+
+  //  }
+//}
